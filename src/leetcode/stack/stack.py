d33b5febd44b1504edceccda78cf8ce17629ee06
abhishek <abhishek.soam@visionias.in>
2020-02-12 22:08:50 +0530

    stack

--- src/leetcode/stack/stack.py
+++ src/leetcode/stack/stack.py
@@ -18,3 +18,51 @@ class Solution:
             previous = current
 
         return True
+
+
+"""
+https://leetcode.com/problems/min-stack/
+"""
+
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.list = []
+
+    def push(self, x: int) -> None:
+        self.list.insert(0, x)
+        if x < self.min:
+            self.min = x
+
+    def pop(self) -> None:
+        self.list.pop(0)
+
+    def top(self) -> int:
+        return self.list[0]
+
+    def getMin(self) -> int:
+        min = float("inf")
+        for a in self.list:
+            if a < min:
+                min = a
+
+        return min
+
+
+"""
+https://leetcode.com/problems/lru-cache/
+"""
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+
+    def get(self, key: int) -> int:
+
+    def put(self, key: int, value: int) -> None:
+
+
