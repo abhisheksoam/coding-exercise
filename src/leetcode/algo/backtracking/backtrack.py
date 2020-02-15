ab1b3ea975a6d114b880a0e95392497f8a3796ef
abhishek <abhishek.soam@visionias.in>
2020-02-15 15:04:23 +0530

    code

--- src/leetcode/algo/backtracking/backtrack.py
+++ src/leetcode/algo/backtracking/backtrack.py
@@ -22,28 +22,42 @@ class Solution:
         return res
 
     """
-    https://leetcode.com/problems/subsets/
+    https://leetcode.com/problems/combination-sum/
     """
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
+        dict = {}
 
         def backtrack(list, target, tmp=[]):
-            if target == 0:
+            if target == 0 and dict.get(
+                    tuple(sorted(tmp))
+            ) is None:
                 res.append(tmp.copy())
+                dict[tuple(
+                    sorted(tmp)
+                )] = True
 
             if target < 0:
                 return
 
-            tmp = []
             for i in range(0, len(list)):
                 element = list[i]
                 tmp.append(element)
                 backtrack(list, target - element, tmp)
                 tmp.remove(element)
 
+        candidates.sort()
         backtrack(candidates, target)
         return res
+
+    """
+    https://leetcode.com/problems/subsets/
+    """
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        pass
+
 
 
 s = Solution()
